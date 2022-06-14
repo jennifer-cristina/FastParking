2740bfd71f67f1e077c855239a30324800aff363
Victor Aguiar <cnhaguiar@gmail.com>
2022-06-14 00:48:04 -0300

    14/06

--- javascript/entradaVeiculo.js/entradaVeiculo.js
+++ javascript/entradaVeiculo.js/entradaVeiculo.js
@@ -1,5 +1,38 @@
 'use strict'
 
+
+
+const readVaga = async () => {
+    const urlVaga = 'https://fast-parking-senai.herokuapp.com/api/vaga'
+    const response = await fetch (urlVaga)
+    const data = await response.json()
+    return data
+}
+
+const readVeiculo = async (id='') => {
+    const idVeiculo = id ? `/${id}` :  ''
+    const urlVeiculo = 'https://fast-parking-senai.herokuapp.com/api/veiculo'
+    const response = await fetch (`${urlVeiculo}${idVeiculo}`)
+    const data = await response.json()
+    return data
+}
+
+const createControle = async (controle) => {
+
+    //url
+    const url = 'https://fast-parking-senai.herokuapp.com/api/controle'
+    const options = {
+        'method': 'POST',
+        'body': JSON.stringify(controle),
+        'headers': {
+            'content-type': 'application/json'
+        }
+    }
+    const response = await fetch(url, options)
+    console.log (response.ok)
+}
+
+
 const dataAtual = () => {
     var data = new Date();
 
@@ -18,6 +51,7 @@ const horario = () => {
     const porteVeiculo = document.getElementById('dataEntrada')
     const horas = dataAtual
 
+
     porteVeiculo.innerHTML = `
         <option>Selecione a data</option>
         <option>
@@ -29,42 +63,49 @@ const horario = () => {
 
 horario()
  
-const placa = document.getElementById('placa')
+// const placa = document.getElementById('placa')
 
-const carregarClientes = async() => {
+// console.log(placa.value)
 
-    const container = document.getElementById('imagem-container')
-    const raca = document.getElementById('raca').value
-    const imagens = await pesquisarDog(raca)
+// // const carregarClientes = async() => {
 
-    const tagImagens = imagens.message.map(criarImg)
+// //     const container = document.getElementById('imagem-container')
+// //     const raca = document.getElementById('raca').value
+// //     const imagens = await pesquisarDog(raca)
 
-    container.replaceChildren(...tagImagens)
+// //     const tagImagens = imagens.message.map(criarImg)
+
+// //     container.replaceChildren(...tagImagens)
+// // }
+
+// const procurarPlaca = async(placa) => {
+
+//     // Colocanda o caminho da URL
+//     const  url = `https://fast-parking-senai.herokuapp.com/api/veiculo/placa/${placa}`
+
+//     // Await: Espera vir o resultado do Fetch para dar a resposta
+//     // Fetch faz a requisição da url
+//     const response = await fetch(url)
+
+//     // Await: Espera vir o resultado do Fetch para dar a resposta
+//     // const data irá receber a resposta em Json()
+//     const data = await response.json()
+
+//     return data
+// } 
+
+// const preencherFormulario = async() => {
+
+//     // Enderco vai receber um método que recebe o valor do CEP
+//     const infoClientes = await procurarPlaca(placa.textContent)
+//     console.log(infoClientes)
+
+//     return document.getElementById(`nomeCliente`).textContent = '4657-ajsh'
+
+// }
+
+// placa.addEventListener('change', preencherFormulario)
+
+export {
+    readVaga, readVeiculo,createControle
 }
-
-const procurarPlaca = async(placa) => {
-
-    // Colocanda o caminho da URL
-    // const  url = `https://viacep.com.br/ws/${cep}/json`
-
-    // Await: Espera vir o resultado do Fetch para dar a resposta
-    // Fetch faz a requisição da url
-    const response = await fetch(url)
-
-    // Await: Espera vir o resultado do Fetch para dar a resposta
-    // const data irá receber a resposta em Json()
-    const data = await response.json()
-
-    return data
-} 
-
-const preencherFormulario = async() => {
-
-    // Enderco vai receber um método que recebe o valor do CEP
-    const infoClientes = await procurarPlaca(placa.value)
-    document.getElementById(`nomeCliente`).value = infoClientes.nomeCliente
-    document.getElementById(`tamanhoVeiculo`).value = infoClientes.tamanhoVeiculo
-}
-
-placa.addEventListener('focusout', preencherFormulario)
-
